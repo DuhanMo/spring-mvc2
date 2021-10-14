c778c76a57780d4fd87dda25515e6dad949d6714
Duhanmo <devduhanmo@gmail.com>
2021-10-14 12:58:43 +0900

    add: thymeleaf block, 주석, 조건, javascript-inline

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -18,33 +18,35 @@ import java.util.Map;
 @RequestMapping("/basic")
 public class BasicController {
 
-    @GetMapping("/text-basic")
+    @GetMapping("text-basic")
     public String textBasic(Model model) {
-        model.addAttribute("data", "hello <b>thymeleaf!</b>");
+        model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-basic";
     }
 
-    @GetMapping("/text-unescaped")
+    @GetMapping("text-unescaped")
     public String textUnescaped(Model model) {
-        model.addAttribute("data", "hello <b>thymeleaf!</b>");
+        model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
     }
 
     @GetMapping("/variable")
     public String variable(Model model) {
-        User userA = new User("userA", 12);
-        User userB = new User("userB", 21);
-        List<User> userList = new ArrayList<>();
-        userList.add(userA);
-        userList.add(userB);
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
 
-        Map<String, User> userMap = new HashMap<>();
-        userMap.put("userA", userA);
-        userMap.put("userB", userB);
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
 
         model.addAttribute("user", userA);
-        model.addAttribute("users", userList);
-        model.addAttribute("userMap", userMap);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
         return "basic/variable";
     }
 
@@ -67,23 +69,24 @@ public class BasicController {
         return "basic/date";
     }
 
-    @GetMapping("/link")
+    @GetMapping("link")
     public String link(Model model) {
         model.addAttribute("param1", "data1");
         model.addAttribute("param2", "data2");
         return "basic/link";
+
     }
 
     @GetMapping("/literal")
     public String literal(Model model) {
-        model.addAttribute("data", "spring");
+        model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
 
     @GetMapping("/operation")
     public String operation(Model model) {
-        model.addAttribute("data", "데이터는스프링");
-        model.addAttribute("nullData", null );
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
 
@@ -94,19 +97,47 @@ public class BasicController {
 
     @GetMapping("/each")
     public String each(Model model) {
-        User userA = new User("모두한", 27);
-        User userB = new User("홍길동", 45);
-        User userC = new User("이순신", 55);
-        User userD = new User("이정재", 48);
-
-        List<User> users = new ArrayList<>();
-        users.add(userA);
-        users.add(userB);
-        users.add(userC);
-        users.add(userD);
-        model.addAttribute("users", users);
+        addUsers(model);
         return "basic/each";
     }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
     @Data
     static class User {
         private String username;
