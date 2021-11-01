eac8c0e2af1fcf3be3b1d13a60d7deff24c0a8c9
moduhan <devduhanmo@gmail.com>
2021-11-01 17:14:32 +0900

    add: BeanValidation groups 이용 -> 실무에서는 잘 이용하지 않음

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -12,7 +12,6 @@ import org.springframework.validation.DataBinder;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
