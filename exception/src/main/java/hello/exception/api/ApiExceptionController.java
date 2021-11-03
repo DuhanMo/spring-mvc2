85516deae8834c12a8852dc7a1191f04c48a9ac7
moduhan <devduhanmo@gmail.com>
2021-11-03 10:32:55 +0900

    add: API 예외처리 - 스프링이 제공하는 ExceptionResolver (2순위 ResponseStatusExceptionResolver)

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,18 +1,21 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class ApiExceptionController {
 
-    @GetMapping("/api/members/{id}")
+    @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
@@ -24,6 +27,11 @@ public class ApiExceptionController {
             throw new UserException("사용자 오류");
         }
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data
